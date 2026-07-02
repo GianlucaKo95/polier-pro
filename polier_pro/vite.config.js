0ad80bf655799844a7cd0cacb626a9c62e631ebc
GianlucaKo95 <gianluca.koehni@googlemail.com>
2026-07-02 12:43:01 +0200

    REWORK

--- polier_pro/vite.config.js
+++ polier_pro/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
     target: ["es2015", "safari14"],
-    minify: "terser",
   },
 });
