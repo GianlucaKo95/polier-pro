710d310613b381b86e8b3ae47c33050ae5c2f93a
GianlucaKo95 <gianluca.koehni@googlemail.com>
2026-07-02 11:49:37 +0200

    FIX

--- polier_pro/src/main.jsx
+++ polier_pro/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import PolierApp from "./App.jsx";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   React.createElement(PolierApp)
 );
